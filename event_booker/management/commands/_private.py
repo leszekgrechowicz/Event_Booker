1f757f0c33b4879e6aed434d3fcc3480c461645a
Leszek Grechowicz <leszek_grechowicz@o2.pl>
2021-12-22 12:22:06 +0000

    Continue to implement database autopopulate

--- event_booker/management/commands/_private.py
+++ event_booker/management/commands/_private.py
@@ -1,5 +1,28 @@
-from event_booker import models
+# from event_booker import models
+from faker import Faker
+import datetime
 
-def create_events():
-    models.Event.objects.create(name='Python Basics', date='20')
-    pass
+fake = Faker()
+
+
+def dates():
+    dates_ = []
+    while len(dates_) < 1:
+        start_date = datetime.date(year=2022, month=1, day=1)
+        date1 = fake.date_between(start_date=start_date, end_date='+2y')
+        date2 = fake.date_between(start_date=start_date, end_date='+2y')
+        if date1 < date2:
+            date3 = date2
+            date3 += datetime.timedelta(days=1)
+            dates_.append((date1, date2, date3))
+    print('success')
+    print(dates_)
+    return dates_
+
+
+# def create_events():
+#     models.Event.objects.create(name='Python Basics', date='20')
+#     pass
+
+if __name__ == '__main__':
+    print(dates())
