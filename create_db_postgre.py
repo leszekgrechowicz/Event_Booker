25cce3c1d545f65afbf9207b6cfdaa1ef3b47bf3
Leszek Grechowicz <leszek_grechowicz@o2.pl>
2022-01-22 08:56:39 +0000

    Delete migrations

--- create_db_postgre.py
+++ create_db_postgre.py
@@ -1,40 +1,53 @@
 from psycopg2 import connect, OperationalError
 
-db_name = "event_booker"
-
-db_choice = input("\nWelcome in film_Web DataBase creator !\n"
-                  "\n>>> ")
-
-connected = False
-while not connected:
-
-    user = input("Please provide Data-Base user name: ")
-    password = input("Password: ")
-
-    try:
-        cnx = connect(user=f"{user}", password=f"{password}", host="localhost")
-        cnx.autocommit = True
-        connected = True
-
-    except OperationalError as error:
-        print(f"Error ! --> {error} ")
+db_name = "event-booker"
 
 
-db_created = False
-while not db_created:
+def db_connect():
+    connected = False
+    while not connected:
 
-    print("You are successfully connected to the database !")
+        user = input("Please provide Data-Base user name: ")
+        password = input("Password: ")
 
-    sql = f"CREATE DATABASE {db_name};"
+        try:
+            cnx = connect(user=f"{user}", password=f"{password}", host="localhost")
+            cnx.autocommit = True
+            connected = True
 
-    try:
-        cursor = cnx.cursor()
-        cursor.execute(sql)
-        print(f"Database {db_name} has been created.")
-        db_created = True
+        except OperationalError as error:
+            print(f"Error ! --> {error} ")
 
-    except OperationalError as error:
-        print(f"Error ! --> {error} ")
+    db_created = False
+    while not db_created:
+
+        print("You are successfully connected to the database !")
+
+        sql = f"CREATE DATABASE {db_name};"
+
+        try:
+            cursor = cnx.cursor()
+            cursor.execute(sql)
+            print(f"Database named: '{db_name}' has been created.")
+
+        except OperationalError as error:
+            print(f"Error ! --> {error} ")
+        else:
+            cursor.close()
+            cnx.close()
+            break
+
+
+print("\nWelcome in Event-Booker Data-Base creator !\n"
+      "----------------------------------------\n"
+      "I am going to create DB for you - DB name => event-booker. ")
+
+not_accepted = True
+while not_accepted:
+    confirmation = input('\nDo you want to proceed ? y/n -> enter\n>>> ')
+    if confirmation.lower() == 'y':
+        db_connect()
+        not_accepted = False
     else:
-        cursor.close()
-        cnx.close()
+        print('Thank you for using our DB creator, Good bye !')
+        not_accepted = False
